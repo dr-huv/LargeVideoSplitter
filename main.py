2d1373b39cb7f7d57e7aab535e3c09f286b6d897
TheViralClovers <theviralclovers@gmail.com>
2022-10-15 23:24:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from get_large_video_files import get_large_video_files
 
-spath = r'C:\Torrents\Courses\The Web Developer Bootcamp 2022'
+spath = r'PATH_TO_YOUR_FILE'
 
 print(get_large_video_files(spath))
